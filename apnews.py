58ddd1602e5ef68423776f0fb081983b163720f3
Aleksander Vall <aleksander.vall@flowit.ee>
2024-06-20 14:06:43 +0300

    Initial commit

---
+++ apnews.py
@@ -0,0 +1,169 @@
+import logging
+from enum import Enum
+from datetime import datetime, date
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.common.exceptions import NoSuchElementException
+
+logger = logging.getLogger(__name__)
+
+
+class Robot:
+
+    class SearchCategory(Enum):
+        STORIES = "Stories"
+        SUBSECTIONS = "Subsections"
+        VIDEOS = "Videos"
+
+
+    def __init__(self, timeout: int=10, browser_width: int=1280, browser_height: int=720, browser_log_level: int=3):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("disable-gpu")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument(f"--log-level={browser_log_level}")
+
+        self.browser = webdriver.Chrome(options=chrome_options)
+        self.browser.set_window_size(browser_width, browser_height)
+
+        self.wait = WebDriverWait(self.browser, timeout=timeout)
+
+        self.browser.get("https://apnews.com/")
+
+        self.accept_cookies()
+
+
+    def stop(self):
+        self.browser.quit()
+
+
+    def take_screenshot(self, file_path: str):
+        self.browser.get_screenshot_as_file(file_path)
+
+
+    def accept_cookies(self):
+        try:
+            accept_cookies_button = self.wait.until(expected_conditions.presence_of_element_located(
+                (By.ID, "onetrust-accept-btn-handler")
+            ))
+            accept_cookies_button.click()
+        except NoSuchElementException as exception:
+            logger.debug(f"NoSuchElementException: {exception}")
+            logger.warning(f"Accept cookies button not found")
+
+
+    def search(self, search_phrase: str, from_date: date) -> list:
+        result_list = list()
+
+        search_button = self.wait.until(expected_conditions.presence_of_element_located(
+            (By.CSS_SELECTOR, "button.SearchOverlay-search-button")
+        ))
+        search_button.click()
+
+        search_input = self.wait.until(expected_conditions.presence_of_element_located(
+            (By.CSS_SELECTOR, "input.SearchOverlay-search-input")
+        ))
+        search_input.send_keys(search_phrase + Keys.ENTER)
+
+        try:
+            search_filter = self.wait.until(expected_conditions.presence_of_element_located(
+                (By.CSS_SELECTOR, "div.SearchFilter")
+            ))
+        except Exception:
+            logger.debug("Search results not found")
+            return result_list
+
+        search_filter_heading = search_filter.find_element(By.CSS_SELECTOR, "div.SearchFilter-heading")
+        search_filter_heading.click()
+
+        try:
+            search_filter_category = search_filter.find_element(
+                By.XPATH, f"//span[contains(text(), '{self.SearchCategory.STORIES.value}')]"
+            )
+            search_filter_category.click()
+        except NoSuchElementException:
+            logger.warning(f"Search category {self.SearchCategory.STORIES.value} does not exist, will ignore it")
+
+
+        first_article = self.wait.until(expected_conditions.presence_of_element_located(
+            (By.CSS_SELECTOR, "div.SearchResultsModule-results div.PageList-items-item")
+        ))
+
+        logger.debug("Found at least one article")
+
+        sort_by_selectbox = self.wait.until(expected_conditions.presence_of_element_located((By.NAME, 's')))
+        Select(sort_by_selectbox).select_by_visible_text('Newest')
+
+        logger.debug("Order latest news first")
+
+        self.wait.until(expected_conditions.staleness_of(first_article))
+
+        result_list = self._add_news_to_list(result_list, from_date)
+
+        logger.debug(result_list)
+
+        return result_list
+
+
+    def _add_news_to_list(self, result_list: list, from_date: date) -> list:
+        news_element_list = self.wait.until(expected_conditions.visibility_of_all_elements_located(
+            (By.CSS_SELECTOR, "div.SearchResultsModule-results div.PageList-items-item")
+        ))
+
+        first_news_dict = self._parse_news_data(news_element_list[0])
+        if first_news_dict.get("date") < from_date:
+            return result_list
+
+        for news_element in news_element_list:
+            try:
+                news_dict = self._parse_news_data(news_element)
+
+                if news_dict.get("date") < from_date:
+                    continue
+
+                result_list.append(news_dict)
+
+            except Exception as error:
+                logger.debug(f"Parsing data error: {error}")
+                logger.warning(f"Problem when parsing news data, will continue with next news")
+
+        try:
+            next_page_button = self.browser.find_element(By.CSS_SELECTOR, "div.Pagination-nextPage a")
+            next_page_button.click()
+        except NoSuchElementException:
+            logger.debug("No more pages found")
+            return result_list
+
+        return self._add_news_to_list(result_list, from_date)
+
+
+    @staticmethod
+    def _parse_news_data(news_element: webdriver.remote.webelement.WebElement) -> dict:
+        timestamp = news_element.find_element(By.CSS_SELECTOR, "bsp-timestamp").get_attribute("data-timestamp")
+        timestamp_seconds = int(timestamp) / 1000
+        news_time = datetime.fromtimestamp(int(timestamp_seconds))
+
+        news_dict = {
+            "title": news_element.find_element(By.CSS_SELECTOR,
+                                               "div.PagePromo-title span.PagePromoContentIcons-text").text,
+            "date": news_time.date(),
+        }
+
+        try:
+            news_dict["description"] = news_element.find_element(
+                By.CSS_SELECTOR, "div.PagePromo-description span.PagePromoContentIcons-text"
+            ).text
+        except NoSuchElementException:
+            news_dict["description"] = ""
+
+        try:
+            news_dict["image_url"] = news_element.find_element(By.CSS_SELECTOR, "img").get_attribute("src")
+        except NoSuchElementException:
+            news_dict["image_url"] = None
+
+        return news_dict
---
+++ tasks.py
@@ -0,0 +1,123 @@
+import os
+import re
+import io
+import uuid
+import logging
+import requests
+from PIL import Image
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+from apnews import Robot as ApnewsRobot
+
+from robocorp.tasks import task
+from RPA.Excel.Files import Files as Excel
+from RPA.Browser.Selenium import Selenium
+
+logger = logging.getLogger(__name__)
+
+SEARCH_PHRASE = os.getenv("SEARCH_PHRASE", "automation")
+NUMBER_OF_MONTHS = os.getenv("NUMBER_OF_MONTHS", 1)
+OUTPUT_PATH = os.getenv("ROBOT_ARTIFACTS", "output")
+NEWS_EXCEL_PATH = os.path.join(OUTPUT_PATH, os.getenv("NEWS_EXCEL_FILENAME", "news.xlsx"))
+DOWNLOAD_IMAGE_PATH = os.path.join(OUTPUT_PATH, os.getenv("DOWNLOAD_IMAGE_DIR", ""))
+
+
+@task
+def search_news():
+    logging.basicConfig(level=logging.INFO)
+
+    starting_date = (datetime.now() - relativedelta(months=max(0, (int(NUMBER_OF_MONTHS) - 1)))).replace(day=1).date()
+
+    logger.info(f"Started searching news for phrase '{SEARCH_PHRASE}' from {starting_date}")
+
+    apnews_robot = ApnewsRobot()
+
+    try:
+        result_list = apnews_robot.search(SEARCH_PHRASE, starting_date)
+
+        logger.info(f"News found: {len(result_list)}")
+
+        if result_list:
+            save_excel(result_list)
+
+    except Exception as error:
+        apnews_robot.take_screenshot(os.path.join("output", "error.png"))
+        logger.error(error)
+
+    finally:
+        logger.info("Searching news completed")
+        apnews_robot.stop()
+
+
+def save_excel(result_list: list):
+    excel_data = {
+        "title": [],
+        "date": [],
+        "description": [],
+        "picture_filename": [],
+        "count_of_search_phrases": [],
+        "has_money_amount": []
+    }
+
+    logger.info("Preparing news data for Excel")
+
+    for news_dict in result_list:
+        excel_data["title"].append(news_dict.get("title", ""))
+        excel_data["date"].append(news_dict.get("date", ""))
+        excel_data["description"].append(news_dict.get("description", ""))
+
+        news_dict["image_file"] = None
+        if news_dict.get("image_url"):
+            try:
+                image_file = download_image(news_dict.get("image_url"), DOWNLOAD_IMAGE_PATH)
+                excel_data["picture_filename"].append(image_file)
+            except Exception as error:
+                excel_data["picture_filename"].append(f"PICTURE DOWNLOAD FROM {news_dict.get('image_url')} FAILED")
+                logger.warning(f"Problem when downloading image: {error}")
+        else:
+            excel_data["picture_filename"].append("")
+
+        count_of_search_phrases = count_search_phrases(
+            SEARCH_PHRASE, news_dict.get("title", ""), news_dict.get("description", "")
+        )
+        excel_data["count_of_search_phrases"].append(count_of_search_phrases)
+
+        has_money_amount_value = has_money_amount(
+            news_dict.get("title", ""), news_dict.get("description", "")
+        )
+        excel_data["has_money_amount"].append(has_money_amount_value)
+
+    logger.info("Adding data to Excel file")
+
+    excel = Excel()
+    excel.create_workbook(path=NEWS_EXCEL_PATH, fmt="xlsx")
+    excel.append_rows_to_worksheet(excel_data, header=True)
+    excel.save_workbook()
+
+
+def download_image(url: str, download_path: str) -> str:
+    response = requests.get(url)
+    response.raise_for_status()
+
+    image = Image.open(io.BytesIO(response.content))
+
+    if download_path:
+        os.makedirs(download_path, exist_ok=True)
+
+    filename = f"{uuid.uuid4()}.jpg"
+    file_path = os.path.join(download_path, filename)
+
+    image.save(file_path, format="JPEG")
+
+    return filename
+
+
+def count_search_phrases(search_phrase: str, *args) -> int:
+    return " ".join(args).count(search_phrase)
+
+
+def has_money_amount(*args) -> bool:
+    regex_pattern = r"(\$\d{1,3}(,\d{3})*(\.\d{1,2})?(?=\s)|(\d+(\.\d{1,2})?( dollars| USD)))"
+    result = re.search(regex_pattern, " ".join(args))
+    return bool(result)
